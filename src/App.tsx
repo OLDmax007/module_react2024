cdf40c134cee4ec6b84d19444d6838d0c48aa0e1
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-10-15 16:59:21 +0300

    Cleaned up files in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
 import React, {FC} from 'react';
 import './App.css';
-import Dog  from './components/Dog'
 
 const App: FC = () => {
   return (
       <div className="App">
-        <h1>DOGS</h1>
-        <Dog fname={'Mikle'}
-              lname={'Chort'}
-              img={'https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg'}
-
-        >
-        </Dog>
-
-        <Dog fname={'Mig'}
-              lname={'Lond'}
-              img={'https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg'}
-        >
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut doloribus itaque nam sunt tempora? Ab
-            ad amet animi atque consectetur, consequuntur culpa cumque debitis dicta eaque error est explicabo,
-            illum impedit laboriosam laudantium libero magni nemo nulla, numquam quasi qui quo rem repudiandae
-            sapiente sequi tempore temporibus veritatis voluptas voluptate!</p>
-        </Dog>
-        {/*<Comp fname='Mikle' lname='Chort'></Comp>*/}
-        {/*<Comp lname={{'A': 1}}></Comp>*/}
-        {/*<Comp lname={10}></Comp>*/}
-        {/*{Comp('Mikle')}*/}
-        {/*{Comp(4308)}*/}
-
       </div>
   );
 }
