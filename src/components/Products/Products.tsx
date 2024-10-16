4508e4dd9f06d7ffacc3c83a143c44aa4d250fa6
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-10-16 16:42:21 +0300

    created css file and rewrote comp Products

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,39 +1,40 @@
 import React, {FC} from 'react';
-import {apiProducts} from "./dataProducts";
+import {apiProducts} from './dataProducts';
 import {IProduct} from '../../models/IProduct'
-import Product from "../Product/Product";
+import Product from '../Product/Product';
+import './Products.css'
 
 const Products: FC = () => {
     const products: IProduct[] = apiProducts.products;
+    console.log(products[0])
     return (
-        <div>
-            <div>
-                {products.map((product: IProduct) => (
-                    <Product
-                        id={product.id}
-                        title={product.title}
-                        description={product.description}
-                        category={product.category}
-                        price={product.price}
-                        discountPercentage={product.discountPercentage}
-                        rating={product.rating}
-                        stock={product.stock}
-                        tags={product.tags}
-                        brand={product.brand}
-                        sku={product.sku}
-                        weight={product.weight}
-                        dimensions={product.dimensions}
-                        warrantyInformation={product.warrantyInformation}
-                        shippingInformation={product.shippingInformation}
-                        availabilityStatus={product.availabilityStatus}
-                        reviews={product.reviews}
-                        returnPolicy={product.returnPolicy}
-                        minimumOrderQuantity={product.minimumOrderQuantity}
-                        // meta={product.meta}
-                        thumbnail={product.thumbnail}
-                    />
-                    ))}
-            </div>
+        <div className='products'>
+            {products.map((product: IProduct) => (
+                // <Product
+                //     id={product.id}
+                //     title={product.title}
+                //     description={product.description}
+                //     category={product.category}
+                //     price={product.price}
+                //     discountPercentage={product.discountPercentage}
+                //     rating={product.rating}
+                //     stock={product.stock}
+                //     tags={product.tags}
+                //     brand={product.brand}
+                //     sku={product.sku}
+                //     weight={product.weight}
+                //     dimensions={product.dimensions}
+                //     warrantyInformation={product.warrantyInformation}
+                //     shippingInformation={product.shippingInformation}
+                //     availabilityStatus={product.availabilityStatus}
+                //     reviews={product.reviews}
+                //     returnPolicy={product.returnPolicy}
+                //     minimumOrderQuantity={product.minimumOrderQuantity}
+                //     // meta={product.meta}
+                //     thumbnail={product.thumbnail}
+                // />
+                <Product {...product}></Product>
+            ))}
         </div>
     );
 };
