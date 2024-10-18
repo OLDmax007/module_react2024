b66624bbd2b2c2b33beaea942e7793b2b36bba38
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-10-18 16:50:18 +0300

    typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React, {FC, useEffect, useState} from 'react';
 import './App.css';
+import {IUser} from "./models/IUser";
 
 const App: FC = () => {
 
-    const [users, setUsers] = useState<any>([])
+    const [users, setUsers] = useState<IUser[]>([])
 
 
     useEffect(() => {
         fetch('https://dummyjson.com/users')
             .then(res => res.json())
             .then(data => {
+                console.log(data)
                 setUsers(data.users)
             });
     }, []);
 
-    console.log(users)
 
   return (
       <div className="App">
-          {users.map((user: any) => <p key={user.id}>{user.firstName}</p> )}
+          {users.map((user: IUser) => <p key={user.id}>{user.firstName}</p> )}
       </div>
   );
 }
