7b4b88b65dbbb198bbfd746cd83c271fa8797f49
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-10-18 16:11:44 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 
 const App: FC = () => {
+
+    const state = useState()
+    console.log(state) // [undefined or 'param', ƒ]
+
+    const [arr, setArr] = useState([1,2,3,4,5])
+    console.log(arr)
+
+    const [users, setUsers] = useState([])
+    console.log(users)
+
+
+    fetch('https://dummyjson.com/users')
+        .then(res => res.json())
+        .then(data => {
+            setUsers(data)
+            console.log(data)
+        });
+
+
   return (
       <div className="App">
       </div>
