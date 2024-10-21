e86d10ee07024a1aef97ca33a761a0bca2efde8b
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-10-21 18:23:33 +0300

    Upd func lift

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,29 @@ import './App.css';
 import Users from "./components/users/Users";
 import {IUser} from "./models/IUser";
 
+
 const App: FC = () => {
 
-   const lift = (user: IUser) => {
-       console.log(user.id)
-   }
-    
-    
-    
+    const lift = (user: IUser) => {
+        fetch('https://dummyjson.com/posts?limit=0')
+            .then(response => response.json())
+            .then(response => {
+
+                const posts = response.posts
+                const filteredPosts = posts.filter((post: any) => {
+                    return post.userId === user.id
+                })
+
+                console.log(...filteredPosts)
+
+            });
+    }
+
+
     return (
         <div className="App">
 
-          <Users lift={lift}/>
+            <Users lift={lift}/>
         </div>
     );
 }
