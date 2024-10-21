3d193591644fea95991d343a7e495dad8e7e940f
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-10-21 17:09:10 +0300

    Created interfaces

---
+++ src/models/IUser.ts
@@ -0,0 +1,25 @@
+interface IHair {
+    color: string,
+    type: string
+}
+
+
+export interface IUser {
+     id : number,
+    firstName: string,
+     lastName :  string,
+     maidenName :  string ,
+     age : number,
+     gender :  string,
+     email :  string ,
+     phone :  string,
+     username :  string ,
+     password :  string ,
+     birthDate :  string ,
+     image :  string ,
+     bloodGroup :  string,
+     height : number,
+     weight : number,
+     eyeColor :  string ,
+     hair : IHair
+}
