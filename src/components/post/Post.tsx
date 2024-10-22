fc5be27e3bf7e6ab8e2a6228e87d77073e63fe86
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-10-22 16:30:20 +0300

    final result (upd)

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -7,18 +7,13 @@ type TypePost = {
 
 const Post: FC<TypePost> = ({post}) => {
     const {id, title, views} = post
-
-
     return (
         <div>
-            {
-
-                    <div className={'postOfCurUser'} key={id}>
-                        <h4>---Num of Post: {id}</h4>
-                        <p>Title: {title}</p>
-                        <p>Number of views: {views}</p>
-                    </div>
-            }
+            <div className={'postOfCurUser'} key={id}>
+                <h4>---Num of Post: {id}</h4>
+                <p>Title: {title}</p>
+                <p>Number of views: {views}</p>
+            </div>
         </div>
     );
 };
--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -10,21 +10,16 @@ type TypePosts = {
 
 const Posts: FC<TypePosts> = ({posts, currUserId}) => {
 
-
     return (
         <div>
             {currUserId !== null && <h3>User: {currUserId}</h3>}
-            {posts.length > 0 ?
-                (
-                    posts.map(({id, title, views}: IPost) => (
-                        <div className={'postOfCurUser'} key={id}>
-                            <h4>---Num of Post: {id}</h4>
-                            <p>Title: {title}</p>
-                            <p>Number of views: {views}</p>
-                        </div>))
-                ) : (
-                    currUserId !== null && <p>User with <strong>id: {currUserId}</strong> hasn't posts</p>
-                )}
+            {posts.length > 0 ? (
+                posts.map((post: IPost) => (
+                    <Post key={post.id} post={post} />
+                ))
+            ) : (
+                currUserId !== null && <p>User with <strong>id: {currUserId}</strong> hasn't posts</p>
+            )}
         </div>
     );
 };
