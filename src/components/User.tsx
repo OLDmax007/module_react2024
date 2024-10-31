703cbdd30054a71a801716f730c747acc1ace132
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-10-31 02:55:44 +0200

    Created page User and upd comp User and also add new route to router

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,16 +1,18 @@
 import React, {FC} from 'react';
 import {IUser} from "../models/IUser";
-import {usersSerive} from "../services/api.dummyjson.service";
+import {Link} from "react-router-dom";
 
 type UserProps = {
     user: IUser
 }
 
 
-const User:FC<UserProps> = ({user: {id, firstName, lastName}}) => {
+const User: FC<UserProps> = ({user: {id, firstName, lastName}}) => {
     return (
         <div>
-            <h3>{id} {firstName} {lastName}</h3>
+            <Link to={`/users/${id}`}>
+                <h3>{id} {firstName} {lastName}</h3>
+            </Link>
         </div>
     );
 };
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,12 +3,14 @@ import MainLayout from "../layouts/MainLayout";
 import ErrorLayout from "../layouts/ErrorLayout";
 import HomePage from "../pages/HomePage";
 import UsersPage from "../pages/UsersPage";
+import UserPage from "../pages/UserPage";
 
 export const router = createBrowserRouter(
     [
         {path:'/', element: <MainLayout/>, children: [
                 {index:true, element:<HomePage/>},
-                {path:'users', element: <UsersPage/>}
+                {path:'users', element: <UsersPage/>},
+                {path:'users/:id', element: <UserPage/>}
             ], errorElement: <ErrorLayout/>}
 
     ]
