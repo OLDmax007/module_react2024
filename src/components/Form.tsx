f0d35dbd285b8c5de465f59202512b2edc4e5c58
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-11-04 16:14:54 +0200

    controlled comp first method

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,21 +2,36 @@ import React, {FormEvent, useState} from 'react';
 
 const Form = () => {
 
-    const [formState, setFormState] = useState<any>({})
+    const [formState, setFormState] = useState<any>({
+        fname: '',
+        lname: '',
+    })
 
     const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const form = e.target as HTMLFormElement;
-        console.log(form.fname.value)
+        console.log(formState)
     }
+
+    const handleChangeFname = (e:FormEvent<HTMLInputElement>) => {
+        const input = e.target as HTMLInputElement;
+        setFormState({...formState, fname: input.value})
+    }
+
+    const handleChangeLname = (e:FormEvent<HTMLInputElement>) => {
+        const input = e.target as HTMLInputElement;
+        setFormState({...formState, lname: input.value})
+    }
+
+
+    console.log(formState)
+
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="">
-                <input type="text" name={'fname'}/>
+                <input type="text" name={'fname'} onChange={handleChangeFname}/>
             </label>
 
             <label htmlFor="">
-                <input type="text" name={'lname'}/>
+                <input type="text" name={'lname'} onChange={handleChangeLname}/>
             </label>
             <button>Ok</button>
         </form>
