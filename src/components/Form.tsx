fc20872ddaece294bb51c330360a65f3a9c3852e
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-11-06 17:28:20 +0200

    Created basic form

---
+++ src/components/Form.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {useForm} from "react-hook-form";
+import {IFormData} from "../models/IFormData";
+
+const Form = () => {
+    const {handleSubmit, register, formState: {errors, isValid}} = useForm<IFormData>();
+
+    const toSubmitForm = (formData) => {
+        console.log(formData)
+    }
+
+    return (
+        <form method={'POST'} onSubmit={handleSubmit(toSubmitForm)}>
+            <label htmlFor="">
+                <input type="text" placeholder={'First name'} {...register('firstname')}/>
+            </label>
+            <label htmlFor="">
+                <input type="text" placeholder={'Last name'} {...register('lastname')}/>
+            </label>
+            <label htmlFor="">
+                <input type="password" placeholder={'Password'}{...register('password')} />
+            </label>
+            <label htmlFor="">
+                <input type="email" placeholder={'Email'} {...register('email')}/>
+            </label>
+            <label htmlFor="">
+                <input type="number" placeholder={'Age'} {...register('age')}/>
+            </label>
+            <label htmlFor="">
+                <input type="radio" value={'single'} {...register('marriedStatus')}/>
+                <p>Single</p>
+            </label>
+            <label htmlFor="">
+                <input type="radio" value={'married'} {...register('marriedStatus')}/>
+                <p>Married</p>
+            </label>
+            <label htmlFor="">
+                <input type="date" {...register('date')}/>
+            </label>
+            <button type={'submit'}>Send</button>
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/models/IFormData.ts
@@ -0,0 +1,9 @@
+export interface IFormData {
+    firstname: string,
+    lastname: string,
+    password: string,
+    age: string,
+    email: string,
+    marriedStatu: 'single' | 'married',
+    birthDate: string;
+}
