17431895066ae5e4e64729a8bcbddee4554c1de3
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-11-06 21:27:52 +0200

    added some field to validator

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,45 +2,80 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 import {IFormData} from "../models/IFormData";
 import {userValidator} from "../validators/user.validator";
+import {joiResolver} from "@hookform/resolvers/joi";
 
 const Form = () => {
     const {handleSubmit,
         register,
-        formState: {errors, isValid}} = useForm<IFormData>(userValidator);
+        formState: {errors, isValid}} =
+        useForm<IFormData>({mode: "all", resolver: joiResolver(userValidator)});
 
     const toSubmitForm = (formData: IFormData) => {
-        console.log(formData)
-    }
+        console.log(formData);
+    };
 
     return (
         <form method={'POST'} onSubmit={handleSubmit(toSubmitForm)}>
-            <label htmlFor="">
-                <input type="text" placeholder={'First name'} {...register('firstname')}/>
-            </label>
-            <label htmlFor="">
-                <input type="text" placeholder={'Last name'} {...register('lastname')}/>
-            </label>
-            <label htmlFor="">
-                <input type="password" placeholder={'Password'}{...register('password')} />
-            </label>
-            <label htmlFor="">
-                <input type="email" placeholder={'Email'} {...register('email')}/>
-            </label>
-            <label htmlFor="">
-                <input type="number" placeholder={'Age'} {...register('age')}/>
-            </label>
-            <label htmlFor="">
-                <input type="radio" defaultChecked={true} value={'single'} {...register('marriedStatus')}/>
-                <p>Single</p>
-            </label>
-            <label htmlFor="">
-                <input type="radio" value={'married'} {...register('marriedStatus')}/>
-                <p>Married</p>
-            </label>
-            <label htmlFor="">
-                <input type="date" placeholder={'Birth date'} {...register('birthDate')}/>
-            </label>
-            <button type={'submit'}>Send</button>
+            <div className="input-block">
+                <label htmlFor="firstname">
+                    <input type="text" placeholder={'First name'} {...register('firstname')}/>
+                </label>
+                {errors.firstname && <div>{errors.firstname.message}</div>}
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="lastname">
+                    <input type="text" placeholder={'Last name'} {...register('lastname')}/>
+                </label>
+                {errors.lastname && <div>{errors.lastname.message}</div>}
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="age">
+                    <input type="number" placeholder={'Age'} {...register('age')}/>
+                </label>
+                {errors.age && <div>{errors.age.message}</div>}
+            </div>
+
+
+            <div className="input-block">
+                <label htmlFor="email">
+                    <input type="email" placeholder={'Email'} {...register('email')}/>
+                </label>
+                {errors.email && <div>{errors.email.message}</div>}
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="password">
+                    <input type="password" placeholder={'Password'} {...register('password')}/>
+                </label>
+                {errors.password && <div>{errors.password.message}</div>}
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="single">
+                    <input type="radio" defaultChecked={true} value={'single'} {...register('marriedStatus')}/>
+                    Single
+                </label>
+                {errors.marriedStatus && <div>{errors.marriedStatus.message}</div>}
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="married">
+                    <input type="radio" value={'married'} {...register('marriedStatus')}/>
+                    Married
+                </label>
+                {errors.marriedStatus && <div>{errors.marriedStatus.message}</div>}
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="birthDate">
+                    <input type="date" placeholder={'Birth date'} {...register('birthDate')}/>
+                </label>
+                {errors.birthDate && <div>{errors.birthDate.message}</div>}
+            </div>
+
+            <button disabled={isValid} type={'submit'}>Send</button>
         </form>
     );
 };
--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,5 +1,53 @@
 import Joi from "joi";
 
 export const userValidator = Joi.object({
-    firstname: Joi.string().required()
+    firstname: Joi.string().min(5).max(15).pattern(new RegExp('\\b[a-z]{5,15}\\b')).required().messages({
+        'any.required': 'Field is required',
+        'string.base': 'First name must be contain only characters',
+        'string.empty': 'First name must be entered',
+        'string.min': 'First name must contain at least 5 characters',
+        'string.max': 'First name must not be longer than 15 characters',
+        'string.pattern.base': 'First name should contain only lower case Latin characters'
+    }),
+    lastname: Joi.string().min(5).max(15).pattern(new RegExp('\\b[a-z]{5,15}\\b')).required().messages({
+        'any.required': 'Field is required',
+        'string.base': 'Last name must be contain only Latin characters',
+        'string.empty': 'Last name is must be entered',
+        'string.min': 'Last name must  contain at least 5 characters',
+        'string.max': 'Last name must  not be longer than 15 characters',
+        'string.pattern.base': 'Last name should contain only lower case Latin characters'
+
+    }),
+    age: Joi.number().min(18).max(60).required().messages({
+        'any.required': 'Field is required',
+        'number.base': 'Age must be number',
+        'number.empty': 'Last name is must be entered',
+        'number.min': 'Age must  be bigger than 18',
+        'number.max': 'Age must  be less than 60'
+    }),
+    password: Joi.string()
+        .min(8)
+        .max(15)
+        .pattern(new RegExp('^[a-zA-Z]{8,10}[@$!%*?&]{1,5}$'))
+        .required()
+        .messages({
+            'any.required': 'Field is required',
+            'string.empty': 'Password must be entered',
+            'string.min': 'Password must  contain at least 8 characters',
+            'string.max': 'Password must not be longer than 15 characters',
+            'string.pattern.base': 'Password must contain only Latin characters with at least one symbol in the end'
+        }),
+    email: Joi.string().email({tlds: {allow: false}}).required().messages({
+        'any.required': 'Field is required',
+        'string.empty': 'Email must be entered',
+        'string.email': 'Email must be a valid email address'
+    }),
+    marriedStatus: Joi.string().required().messages({
+        'any.required': 'Radio is required',
+        'string.empty': 'Married status must be selected',
+    }),
+    birthDate: Joi.string().required().messages({
+        'any.required': 'Field is required',
+        'string.empty': 'Date must be selected',
+    }),
 });
