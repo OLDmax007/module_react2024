6cf0e5cf9067634c1433bb5d936f6a10206f8f61
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-11-14 23:50:27 +0200

    useref

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 const App = () => {
-    let [count, setCount] = useState<number>(0)
 
-    console.log('basic: '+ count)
+    let [count, setCount] = useState<number>(0)
+    let someRef = useRef<number>(0);
 
     useEffect(() => {
         console.log(count)
-
-        // func unsubscribe
-        return (
-            () => {
-                console.log('finish')
-            }
-        )
-
-    }, [count]);
-
-
-
-    const inc = () => {
-        setCount(prevState => prevState+1)
-        setCount(prevState => prevState+1)
-        setCount(prevState => prevState+1)
-    }
-
-    const dec = () => {
-        setCount(prevState => prevState-1)
-    }
-
-
-
+        console.log(someRef.current)
+        console.log(someRef)
+    }, [someRef.current]);
 
 
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={dec}>dec</button>
-            <button onClick={inc}>inc</button>
+            <button onClick={() => {
+                count--
+                console.log(someRef.current++);
+            }}>dec
+            </button>
 
+            <button  onClick={() => {
+                count++
+                console.log(someRef.current--);
+            }}>inc
+            </button>
 
         </div>
     );
