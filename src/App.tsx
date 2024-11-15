43bc56a95b6207ae3b23fb5083072a8d6f0c6fd0
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-11-16 00:51:53 +0200

    class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import User from "./hoc/User";
-import SomeComp from "./SomeComp";
+import React, {Component} from 'react';
 
-const App = () => {
-    return (
-        <div>
-            <User><SomeComp/></User>
-        </div>
-    );
-};
+type StateProps = {
+    todos: []
+}
+
+
+class App extends Component<StateProps> {
+    constructor(props: StateProps) {
+        super(props);
+        this.state = {todos: []}
+    }
+
+
+    componentDidMount() {
+        fetch('https://dummyjson.com/todos')
+            .then(response => response.json())
+            .then(data => {
+                this.setState(data.todos)
+            })
+
+    }
+
+
+    componentWillUnmount() {
+        console.log('finish')
+    }
+
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
 
 export default App;
